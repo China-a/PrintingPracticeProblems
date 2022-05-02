b7c4087e79129b3b7b0bad6180efb0f155d596ad
China-a <China.allford@gmail.com>
2022-05-02 11:50:05 -0400

    Print Java for the first time

---
+++ helloWorld/src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String HiFriend = "Hi " + "Larry Sprinkle!";
+        System.out.println(HiFriend);
+    }
+}
+
